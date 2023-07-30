24931074bbd3a4c9a4d6c89221d5636579b773fa
iamargentum <rajat.rj97@gmail.com>
2023-07-30 16:56:13 +0530

    init gatsby website

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export function Main() {
+
+    return (
+        <div>
+            <p>hello</p>
+            <p>world!</p>
+            <p>i'm</p>
+            <p>rajat</p>
+        </div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Main } from "../components/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Main />
+      
+    </>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
