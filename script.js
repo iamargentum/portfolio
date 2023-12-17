8852603a75722b2f49534b523f27b23f05a040d0
iamargentum <rajat.rj97@gmail.com>
2023-12-17 07:54:59 +0530

    Revert "Purge everything, yet again! XD"
    This reverts commit 8a611d9a79935fdc03fe375e48aa8b722cab5d22.

---
+++ script.js
@@ -0,0 +1,56 @@
+const lifeStrip = document.getElementById("lifeStrip");
+const workStrip = document.getElementById("workStrip");
+const projectsStrip = document.getElementById("projectsStrip");
+const workSectionContent = document.getElementById("workSectionContent");
+const lifeSectionContent = document.getElementById("lifeSectionContent");
+const projectsSectionContent = document.getElementById("projectsSectionContent");
+const slidingWordsParents = document.getElementsByClassName("slidingWordsParent");
+var slidingWordContainers;
+
+if(document.documentElement.scrollTop > window.innerHeight) {
+    document.documentElement.style.setProperty("--slidingWordOpacity", "0");
+}
+
+for (var i = 0; i < slidingWordsParents.length; i++) {
+    const slidingWordsParent = slidingWordsParents.item(i);
+    slidingWordContainers = slidingWordsParent.children;
+}
+
+document.addEventListener("scroll", (e) => {
+    var st = document.documentElement.scrollTop;
+    if(st <= window.innerHeight) {
+        document.documentElement.style.setProperty("--evenBottom", ((st/window.innerHeight).toString() + "em"));
+        document.documentElement.style.setProperty("--oddBottom", "-" + ((st/window.innerHeight).toString() + "em"));
+        document.documentElement.style.setProperty("--slidingWordOpacity", (1 - st/window.innerHeight).toString());
+    }
+
+    if(st >= workSectionContent.offsetTop && st <= projectsSectionContent.offsetTop) {
+        if(!workStrip.classList.contains("topLeftSticky")) {
+            workStrip.classList.add("topLeftSticky");
+        }
+    } else {
+        if(workStrip.classList.contains("topLeftSticky")) {
+            workStrip.classList.remove("topLeftSticky");
+        }
+    }
+
+    if(st >= projectsSectionContent.offsetTop && st <= lifeSectionContent.offsetTop) {
+        if(!projectsStrip.classList.contains("topRightSticky")) {
+            projectsStrip.classList.add("topRightSticky");
+        }
+    } else {
+        if(projectsStrip.classList.contains("topRightSticky")) {
+            projectsStrip.classList.remove("topRightSticky");
+        }
+    }
+
+    if(st >= lifeSectionContent.offsetTop) {
+        if(!lifeStrip.classList.contains("topLeftSticky")) {
+            lifeStrip.classList.add("topLeftSticky");
+        }
+    } else {
+        if(lifeStrip.classList.contains("topLeftSticky")) {
+            lifeStrip.classList.remove("topLeftSticky");
+        }
+    }
+});
